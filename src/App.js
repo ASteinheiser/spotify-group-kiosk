31c5b488a73131a933e2d29744486ab0a0166b44
Andrew Steinheiser <me@iamandrew.io>
2021-10-21 16:25:22 -0700

    implement usePatchbay hook to test rendering data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import usePatchbay from './usePatchbay';
+
+const { REACT_APP_PATCHBAY_PUBSUB_KEY } = process.env;
+const PATCHBAY_URL = `https://patchbay.pub/pubsub/${REACT_APP_PATCHBAY_PUBSUB_KEY}?persist=true`;
+
 const App = () => {
+  const { data, error } = usePatchbay(PATCHBAY_URL);
+
+  console.log({ error });
+
   return (
     <div>
       <h1>Spotify Group Session Kiosk</h1>
+      <h2>{data}</h2>
     </div>
   );
 }
