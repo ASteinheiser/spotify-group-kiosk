fcd6e06818a1fdba2b05976b76cbe97da64457ba
Andrew Steinheiser <me@iamandrew.io>
2021-10-21 23:55:18 -0700

    create a basic router for showing admin controllerand kiosk

---
+++ src/Controller.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+const Controller = ({ apiUrl }) => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [url, setUrl] = useState('');
+
+  return null;
+}
+
+export default Controller;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
+import Kiosk from './Kiosk';
+import Controller from './Controller';
 import './index.css';
+
+const { REACT_APP_PATCHBAY_PUBSUB_KEY } = process.env;
+const PATCHBAY_URL = `https://patchbay.pub/pubsub/${REACT_APP_PATCHBAY_PUBSUB_KEY}`;
+
+const Router = () => {
+  if (window.location.pathname === '/admin') {
+    return <Controller apiUrl={PATCHBAY_URL} />;
+  }
+  return <Kiosk apiUrl={`${PATCHBAY_URL}?persist=true`} />;
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>,
   document.getElementById('root')
 );
