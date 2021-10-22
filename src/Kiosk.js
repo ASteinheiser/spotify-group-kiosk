4e714ad27d410faace9dc284c78902b939c8a469
Andrew Steinheiser <me@iamandrew.io>
2021-10-21 23:53:49 -0700

    rename app to Kiosk and pull out apiUrl as a prop

--- src/Kiosk.js
+++ src/Kiosk.js
@@ -2,11 +2,8 @@ import QRCode from 'react-qr-code';
 
 import usePatchbay from './usePatchbay';
 
-const { REACT_APP_PATCHBAY_PUBSUB_KEY } = process.env;
-const PATCHBAY_URL = `https://patchbay.pub/pubsub/${REACT_APP_PATCHBAY_PUBSUB_KEY}?persist=true`;
-
-const App = () => {
-  const { data, error } = usePatchbay(PATCHBAY_URL);
+const Kiosk = ({ apiUrl }) => {
+  const { data, error } = usePatchbay(apiUrl);
 
   return (
     <div id='wrapper'>
@@ -31,4 +28,4 @@ const App = () => {
   );
 }
 
-export default App;
+export default Kiosk;
