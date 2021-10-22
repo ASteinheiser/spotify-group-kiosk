2e4aa970d90a4845d448569bf150828e7014df75
Andrew Steinheiser <me@iamandrew.io>
2021-10-22 00:02:51 -0700

    basic form for admin controller

--- src/Controller.js
+++ src/Controller.js
@@ -5,7 +5,42 @@ const Controller = ({ apiUrl }) => {
   const [body, setBody] = useState('');
   const [url, setUrl] = useState('');
 
-  return null;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!title || !body || !url) {
+      return alert('Please fill out all fields');
+    }
+
+    console.log('publishing!')
+    // publish message
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Title"
+          type="text"
+          value={title}
+          onChange={({ target }) => setTitle(target.value)}
+        />
+        <input
+          placeholder="Body"
+          type="text"
+          value={body}
+          onChange={({ target }) => setBody(target.value)}
+        />
+        <input
+          placeholder="URL"
+          type="text"
+          value={url}
+          onChange={({ target }) => setUrl(target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default Controller;
