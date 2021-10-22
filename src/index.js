e77506eb03b77698f6772b5df4756263c6265a77
Andrew Steinheiser <me@iamandrew.io>
2021-10-22 12:52:14 -0700

    install query-string and sha256 hash lib for auto generated patchbay keys

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import querystring from 'query-string';
+import sha256 from 'js-sha256';
 
 import Kiosk from './Kiosk';
 import Controller from './Controller';
 import './index.css';
 
-const { REACT_APP_PATCHBAY_PUBSUB_KEY } = process.env;
-const PATCHBAY_URL = `https://patchbay.pub/pubsub/${REACT_APP_PATCHBAY_PUBSUB_KEY}`;
-
 const Router = () => {
-  const { pathname } = window.location;
+  const { pathname, search } = window.location;
+  const query = querystring.parse(search);
+
+  let patchbayKey = query.token;
+  if (!patchbayKey) {
+    console.log(sha256(Math.random().toString()));
+  }
+
+  const patchbayUrl = `https://patchbay.pub/pubsub/${patchbayKey}`;
+
+  console.log({ patchbayUrl });
 
   if (pathname === '/') {
-    return <Kiosk apiUrl={`${PATCHBAY_URL}?persist=true`} />;
+    return <Kiosk apiUrl={`${patchbayUrl}?persist=true`} />;
   }
   if (pathname === '/admin') {
-    return <Controller apiUrl={PATCHBAY_URL} />;
+    return <Controller apiUrl={patchbayUrl} />;
   }
   // redirect to base url if no match
   window.location.href = '/';
